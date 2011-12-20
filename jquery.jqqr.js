da3649279fe70a8bc6953bb34bce1478584269b0
Gun.io Whitespace Robot <contact@gun.io>
2011-12-20 06:15:34 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- jquery.jqqr.js
+++ jquery.jqqr.js
@@ -1,12 +1,12 @@
 (function($){
     $.fn.jqqr = function(options) {
         return this.each(function(){
-          
+
           var ch1 = $(this).html();
           $(this).html('');
-          
+
           var dimensions = $(this).attr('rel').split('x');
-          
+
           $(this).append('<img src="http://chart.apis.google.com/chart?cht=qr&chs='+dimensions[0]+'x'+dimensions[1]+'&chl='+ch1+'">')
         });
     };
